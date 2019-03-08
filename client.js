3794fc9448014ba8675a195ff066afeb37be4156
Lili Bourgeois <lbourgeois90@gmail.com>
2019-03-08 17:48:33 -0600

    Calculates Monlty Costs and Appends it

--- client.js
+++ client.js
@@ -1,11 +1,12 @@
-let totalEmployees = [];
-
+const totalEmployees = [];
+let totalMonthly = 0;
 
 $(document).ready(readyNow);
 
 function readyNow(){
     console.log('in JQ');
     $('#submitButton').on('click', addEmployee);
+
 }
 
 function addEmployee(){
@@ -18,13 +19,25 @@ function addEmployee(){
 
     }//end employeeObject
     totalEmployees.push( newEmployee );
+    calculateMonthlyCost();
     displayInTable();
 
 }//end addEmployee
 
 function displayInTable(){
     $('.tableInputs').empty();
+    $('#addMonthlyTotal').empty();
     for (let item of totalEmployees){
         $('.tableInputs').append(`<tr><td>${item.firstName}</td><td>${item.lastName}</td><td>${item.idNumber}</td><td>${item.jobTitle}</td><td>${item.annualSalary}</td></tr>`);
     }//end for loop
+    $('#addMonthlyTotal').append(totalMonthly);
+
 }//end display
+
+function calculateMonthlyCost(){
+    for( let i = 0; i<totalEmployees.length; i++) {
+    totalMonthly = Number(totalEmployees[i].annualSalary) + totalMonthly;
+    }//end for
+    totalMonthly = (totalMonthly / 12);
+  return totalMonthly;
+}//end calculateMonthlyFunction
